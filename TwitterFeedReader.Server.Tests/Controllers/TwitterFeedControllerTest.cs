6476b9c353d459759e250a0dc3c02d0337893355
David Hurd <dhurd77@gmail.com>
2017-10-30 01:47:32 -0400

    added some tests

---
+++ TwitterFeedReader.Server.Tests/Controllers/TwitterFeedControllerTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitterFeedReader.Server.Controllers;
+using TwitterFeedReader.Server.Models;
+
+namespace TwitterFeedReader.Server.Tests.Controllers
+{
+    [TestClass]
+    public class TwitterFeedControllerTest
+    {
+        [TestMethod]
+        public async Task GetTenTweets()
+        {
+            TwitterFeedController controller = new TwitterFeedController();
+            IEnumerable<Tweet> result = await controller.Get(10, "salesforce", "");
+            Assert.IsNotNull(result);
+            Assert.AreEqual(10, result.Count());
+        }
+
+        [TestMethod]
+        public async Task GetWithBadFilter()
+        { 
+            TwitterFeedController controller = new TwitterFeedController();
+            IEnumerable<Tweet> result = await controller.Get(10, "salesforce", "david hurd is awesome");
+            Assert.IsNotNull(result);
+            Assert.AreEqual(0, result.Count());
+        }
+    }
+}
